e4122cfe6e22407609a0035e86410a464886f349
Stady <dstankevich@mail.ru>
2023-12-22 16:23:24 +0300

    sum

--- test.cpp
+++ test.cpp
@@ -2,15 +2,15 @@
 #include <conio.h>
 
 using namespace std;
+
+int sum(int a, int b)
+{
+    return a+b;
+}
+
 int main()
 {
   cout << "Hello!!!" << endl;
+  cout << sum(3,6) << endl;
   getch();
-
-  cout << "getch is pressed!!!" << endl;
-  getch();
-
-
-
-
 }
