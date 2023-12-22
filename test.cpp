78bd525f0a1f7a3a5d4786e8e35be163854e0a96
Stady <dstankevich@mail.ru>
2023-12-22 16:25:33 +0300

    inc

--- test.cpp
+++ test.cpp
@@ -8,9 +8,15 @@ int sum(int a, int b)
     return a+b;
 }
 
+int inc(int a)
+{
+    return ++a;
+}
+
 int main()
 {
   cout << "Hello!!!" << endl;
   cout << sum(3,6) << endl;
+  cout << inc(6) << endl;
   getch();
 }
