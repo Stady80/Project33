fb803030da48ae70292e0ac3b9a48362aeefcccc
Stady <dstankevich@mail.ru>
2023-12-22 16:20:03 +0300

    External console
    commit2
    
    second commit

--- test.cpp
+++ test.cpp
@@ -1,7 +1,16 @@
 #include <iostream>
+#include <conio.h>
 
 using namespace std;
 int main()
 {
   cout << "Hello!!!" << endl;
+  getch();
+
+  cout << "getch is pressed!!!" << endl;
+  getch();
+
+
+
+
 }
